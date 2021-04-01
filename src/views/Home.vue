c8ff4bb0ffecabf5939c8dfeb6795be6a513419b
claudioquin <bmx347@gmail.com>
2021-04-01 10:20:06 -0300

    made navbar hero and first card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  
+    <Navbar/>
+    <Hero/>
+
+    <Planetatooin  />
+     
+
+
 </template>
 
 <script>
+import Hero from '../components/Hero.vue'
+import Navbar from '../components/Navbar.vue'
+import Planetatooin from '../components/Planetatooin.vue'
 
 export default {
   name: 'Home',
   components: {
+     Navbar,
+    Hero,
+    Planetatooin,
    
   }
+
 }
 </script>
