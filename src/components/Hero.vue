b3b15f2fdfc39cfb421b8ce22d3c9913906c731e
claudioquin <bmx347@gmail.com>
2021-04-01 20:06:16 -0300

    some media queries added

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,8 @@
 <template>
     <section class="herototal">
         <div class="herowrap">
-            <img src="https://firebasestorage.googleapis.com/v0/b/cereales-ebbb8.appspot.com/o/tatooine.png?alt=media&token=397215cf-b807-438f-b399-09308cc64fda" alt="imagen-planeta">
+            <img class="stwimg1" src="https://firebasestorage.googleapis.com/v0/b/cereales-ebbb8.appspot.com/o/tatooine.png?alt=media&token=397215cf-b807-438f-b399-09308cc64fda" alt="imagen-planeta">
+            <img class="stwimg" src="img/stwars.png" alt="">
         </div>
 
     </section>
@@ -26,6 +27,32 @@ export default {
                   }
           }
 }
+.stwimg{
+    position: absolute;
+    top: 55%;
+    left: 37%;
+    width: 25%;
+}
 
-</style>>
+
+@media (max-width: 790px){
+        .stwimg{
+         top: 60%;
+         left: 29%;
+        width: 45%;
+}
+
+}
+@media (max-width: 600px){
+    .stwimg{
+    top: 60%;
+    left: 29%;
+    width: 45%;
+
+}
+.stwimg1{
+    height: 74vh;
+}
+}
+</style>
 
