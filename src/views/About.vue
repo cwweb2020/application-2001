93c2af151f9a3065c59ff323664808e50d73540c
claudioquin <bmx347@gmail.com>
2021-04-01 15:05:54 -0300

    added home nav

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
+<div class="totalhommy">
+  <router-link to="/" class="text-white fs-3 hommy">Home</router-link>
+</div>
   <div class="about">
     <h1 class="text-center text-white fs-2 mt-5 pt-5">EXPLORE STAR WORS</h1>
   </div>
 </template>
+<style>
+.totalhommy{
+  margin: 50px;
+
+}
+   .hommy{
+     color: yellow !important;
+     padding: 10px
+   }
+</style>
