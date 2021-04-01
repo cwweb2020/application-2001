04976dec7eb3c9688db9b5b106ed256877c2001e
claudioquin <bmx347@gmail.com>
2021-04-01 10:47:30 -0300

    made fetch and got data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    planets:[]
   },
   mutations: {
+    setPlanets(state, payload){
+      state.planets=payload
+
+      console.log(state.planets);
+    }
   },
   actions: {
+    fetchPlanet({commit}){
+      axios
+      .get('https://swapi.dev/api/planets/')
+      .then(res =>{
+         const datos=res.data
+           commit('setPlanets',datos)
+         console.log(datos);
+         
+          
+      })
+      .catch(e => console.log(e))
+    }
   },
   modules: {
   }
